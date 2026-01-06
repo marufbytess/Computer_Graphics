753941200bff9fb2c3454c40f4c2ada657278be4
marufbytes <marufahammed215@gmail.com>
2026-01-07 02:48:49 +0600

    git

--- Basic/Practice/main.cpp
+++ Basic/Practice/main.cpp
@@ -1,105 +1,38 @@
-#include <GL/glut.h>
+#include<gl/Glut.h>
+#include<cmath>
 
-// Rotation angle
-float angle = 0.0f;
-
-// Function to draw a cube
-void drawCube() {
-    glBegin(GL_QUADS);
-
-    // Front face (Red)
-    glColor3f(1.0, 0.0, 0.0);
-    glVertex3f(-1.0, -1.0,  1.0);
-    glVertex3f( 1.0, -1.0,  1.0);
-    glVertex3f( 1.0,  1.0,  1.0);
-    glVertex3f(-1.0,  1.0,  1.0);
-
-    // Back face (Green)
-    glColor3f(0.0, 1.0, 0.0);
-    glVertex3f(-1.0, -1.0, -1.0);
-    glVertex3f(-1.0,  1.0, -1.0);
-    glVertex3f( 1.0,  1.0, -1.0);
-    glVertex3f( 1.0, -1.0, -1.0);
-
-    // Top face (Blue)
-    glColor3f(0.0, 0.0, 1.0);
-    glVertex3f(-1.0,  1.0, -1.0);
-    glVertex3f(-1.0,  1.0,  1.0);
-    glVertex3f( 1.0,  1.0,  1.0);
-    glVertex3f( 1.0,  1.0, -1.0);
-
-    // Bottom face (Yellow)
-    glColor3f(1.0, 1.0, 0.0);
-    glVertex3f(-1.0, -1.0, -1.0);
-    glVertex3f( 1.0, -1.0, -1.0);
-    glVertex3f( 1.0, -1.0,  1.0);
-    glVertex3f(-1.0, -1.0,  1.0);
-
-    // Right face (Cyan)
-    glColor3f(0.0, 1.0, 1.0);
-    glVertex3f( 1.0, -1.0, -1.0);
-    glVertex3f( 1.0,  1.0, -1.0);
-    glVertex3f( 1.0,  1.0,  1.0);
-    glVertex3f( 1.0, -1.0,  1.0);
-
-    // Left face (Magenta)
-    glColor3f(1.0, 0.0, 1.0);
-    glVertex3f(-1.0, -1.0, -1.0);
-    glVertex3f(-1.0, -1.0,  1.0);
-    glVertex3f(-1.0,  1.0,  1.0);
-    glVertex3f(-1.0,  1.0, -1.0);
-
-    glEnd();
-}
-
-// Display function
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glLoadIdentity();
-    glTranslatef(0.0f, 0.0f, -6.0f); // Move cube into screen
-    glRotatef(angle, 1.0f, 1.0f, 0.0f); // Rotate cube
-
-    drawCube();
-
-    glutSwapBuffers();
-}
-
-// Timer for animation
-void timer(int) {
-    angle += 1.0f;
-    if (angle > 360) angle -= 360;
-    glutPostRedisplay();
-    glutTimerFunc(10, timer, 0);
-}
-
-// Initialization
-void init() {
-    glEnable(GL_DEPTH_TEST); // Enable depth
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-}
-
-// Reshape function
-void reshape(int w, int h) {
-    glViewport(0, 0, w, h);
+void init()
+{
+    glClearColor(0,0,0,0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0, (float)w / h, 1.0, 100.0);
-    glMatrixMode(GL_MODELVIEW);
+    gluOrtho2D(0,20,0,20);
 }
 
-// Main function
-int main(int argc, char** argv) {
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1,0,0);
+    glBegin(GL_LINE_LOOP);
+    for(int i=0;i<360;i++)
+    {
+        float angle = i*3.1416/180;
+        float x = 10+ 5*cos(angle);
+        float y = 10+ 5*sin(angle);
+        glVertex2f(x,y);
+    }
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char **argv)
+{
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(600, 600);
-    glutCreateWindow("3D Cube - OpenGL");
+    glutInitWindowSize(500,500);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutCreateWindow("create");
 
     init();
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutTimerFunc(0, timer, 0);
-
     glutMainLoop();
-    return 0;
 }
